edef5aee61e26ea5efd889776be7de0557ab4e41
prathmeshthorat <prathmeshthorat06@gmail.com>
2021-09-01 23:27:57 +0530

    adding events datasource in order-service

--- order-service/order-service/src/main/java/com/ueats/orderservice/configuration/UEEventsDBSourceConfig.java
+++ order-service/order-service/src/main/java/com/ueats/orderservice/configuration/UEEventsDBSourceConfig.java
@@ -1,5 +1,9 @@
 package com.ueats.orderservice.configuration;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -10,24 +14,41 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
-
 @EnableTransactionManagement
-@EnableJpaRepositories(entityManagerFactoryRef = "entityManagerFactory", basePackages = {"com.ueats.orderservice.events.repository"})
+@EnableJpaRepositories(entityManagerFactoryRef = "entityManagerFactory", basePackages = {
+		"com.ueats.orderservice.events.repository" }, transactionManagerRef = "transactionManager")
 public class UEEventsDBSourceConfig {
-	
+
 	@Primary
-	@Bean(name = "eventsDataSource")
+	@Bean(name = "dataSource")
 	@ConfigurationProperties(prefix = "spring.events.datasource")
 	public DataSource dataSource() {
 		return DataSourceBuilder.create().build();
 	}
-	
+
+	@Primary
+	@Bean(name = "entityManagerFactory")
 	public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean(EntityManagerFactoryBuilder builder,
-			@Qualifier("eventsDataSource") DataSource  eventsDataSource) {
-		return builder.dataSource(eventsDataSource).packages("com.ueats.orderservice.entity").persistenceUnit("")
+			@Qualifier("dataSource") DataSource eventsDataSource) {
+
+		Map<String, Object> properties = new HashMap<>();
+		properties.put("hibernate.hbm2ddl.auto", "none");
+		properties.put("hibernate.dialec", "org.hibernate.dialect.MySQL5Dialect");
+
+		return builder.dataSource(eventsDataSource).packages("com.ueats.orderservice.events.entity").properties(properties)
+				.persistenceUnit("Events").build();
+	}
+
+	@Primary
+	@Bean(name = "transactionManager")
+	public PlatformTransactionManager transactionManager(
+			@Qualifier("entityManagerFactory") EntityManagerFactory entityManagerFactory) {
+		return new JpaTransactionManager(entityManagerFactory);
 	}
 }
--- order-service/order-service/src/main/java/com/ueats/orderservice/configuration/UEOrdersDBSourceConfig.java
+++ order-service/order-service/src/main/java/com/ueats/orderservice/configuration/UEOrdersDBSourceConfig.java
@@ -1,12 +1,54 @@
 package com.ueats.orderservice.configuration;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableTransactionManagement
-@EnableJpaRepositories(entityManagerFactoryRef = "entityManagerFactory", basePackages = {"com.ueats.orderservice.repository"})
+@EnableJpaRepositories(entityManagerFactoryRef = "orderEntityManagerFactory", basePackages = {"com.ueats.orderservice.repository"},
+transactionManagerRef = "orderTransactionManager")
 public class UEOrdersDBSourceConfig {
+	
+	
+	@Bean(name = "orderDataSource")
+	@ConfigurationProperties(prefix = "spring.orders.datasource")
+	public DataSource dataSource() {
+		return DataSourceBuilder.create().build();
+	}
 
+	@Primary
+	@Bean(name = "orderEntityManagerFactory")
+	public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean(EntityManagerFactoryBuilder builder,
+			@Qualifier("orderDataSource") DataSource orderDataSource) {
+
+		Map<String, Object> properties = new HashMap<>();
+		properties.put("hibernate.hbm2ddl.auto", "none");
+		properties.put("hibernate.dialec", "org.hibernate.dialect.MySQL5Dialect");
+
+		return builder.dataSource(orderDataSource).packages("com.ueats.orderservice.entity").properties(properties)
+				.persistenceUnit("Order").build();
+	}
+
+	@Primary
+	@Bean(name = "orderTransactionManager")
+	public PlatformTransactionManager transactionManager(
+			@Qualifier("orderEntityManagerFactory") EntityManagerFactory entityManagerFactory) {
+		return new JpaTransactionManager(entityManagerFactory);
+	}
 }
